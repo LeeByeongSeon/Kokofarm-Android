4152f50acdde8a2a21ec9dbd64ba5b4d115d58e8
josohyeon <josohyun0@naver.com>
2022-07-08 21:54:07 +0900

    페이징 버튼 추가

--- kokofarm_user_app/app/src/main/java/com/example/kokofarm_user_app/OutRecordActivity.java
+++ kokofarm_user_app/app/src/main/java/com/example/kokofarm_user_app/OutRecordActivity.java
@@ -11,7 +11,6 @@ import com.example.kokofarm_user_app.databinding.ActivityOutRecordBinding;
 public class OutRecordActivity extends AppCompatActivity {
 
     private ActivityOutRecordBinding binding;
-    private RecyclerView recyclerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,29 +23,5 @@ public class OutRecordActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.out_txt);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        recyclerView = binding.scaleListView;
-
-        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(getApplicationContext());
-        for(int i=1; i<=20; i++){
-            String str;
-            if(i<10){
-                str = "0"+i;
-            } else {
-                str = String.valueOf(i);
-            }
-            recyclerAdapter.addItem(str);
-        }
-        recyclerView.setAdapter(recyclerAdapter);
-
-//        Spinner spinnerMonth = binding.outMonth;
-//        ArrayList<String> monthArr = new ArrayList<>();
-//        for(int i=1; i<13; i++){
-//            monthArr.add(i+"월");
-//        }
-//
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, monthArr);
-//
-//        spinnerMonth.setAdapter(arrayAdapter);
     }
 }
